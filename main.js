15f7350e6dc1f69ab9ed9a47be6f2289f8b144e5
DESKTOP-P18FM8D\gg <Mahmoud.Khaled.M.96@gmail.com>
2023-04-03 22:22:13 +0200

    init_project

---
+++ main.js
@@ -0,0 +1,52 @@
+let home = document.getElementById("home")
+let skill = document.getElementById("skills")
+let gallary = document.getElementById("gallary")
+let about = document.getElementById("about")
+let contact = document.getElementById("contact")
+let video = document.getElementById("video")
+
+
+console.log(skill, gallary, about, contact)
+
+function getCurrentURL(event) {
+    // console.log(event.target.innerText)
+    let ele = document.getElementsByTagName('span')
+    for (let index = 0; index < ele.length; index++) {
+        if (event.target.innerText === ele[index].innerHTML) {
+            ele[index].setAttribute('class', "text-link active")
+            // console.log(ele[index].innerHTML)
+            // console.log(event.target.innerText)
+            // console.log(ele[index].innerHTML)
+        } else {
+            ele[index].setAttribute('class', "text-link")
+        }
+    }
+}
+
+
+window.addEventListener('scroll', () => {
+    // console.log(document.documentElement.scrollTop)
+    if (document.documentElement.scrollTop < 390) {
+        skill.setAttribute("class", "text-link ")
+        home.setAttribute("class", "text-link active")
+        gallary.setAttribute("class", "text-link")
+        about.setAttribute("class", "text-link")
+        contact.setAttribute("class", "text-link")
+    }
+    else if (document.documentElement.scrollTop >= 390 && document.documentElement.scrollTop < 1012) {
+        skill.setAttribute("class", "text-link active")
+        gallary.setAttribute("class", "text-link")
+        about.setAttribute("class", "text-link")
+        home.setAttribute("class", "text-link")
+        contact.setAttribute("class", "text-link")
+    }
+    else if (document.documentElement.scrollTop >= 1012) {
+        skill.setAttribute("class", "text-link")
+        gallary.setAttribute("class", "text-link active")
+        about.setAttribute("class", "text-link")
+        home.setAttribute("class", "text-link")
+        contact.setAttribute("class", "text-link")
+        // video.style.transform ="translateX(0px)"
+        console.log(video.style)
+    }
+});
